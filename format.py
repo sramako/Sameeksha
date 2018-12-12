f4c1ec6c894d06cb67d7fe5149657b7c65026c3f
Sraman <sramakoo@gmail.com>
2018-12-12 14:21:24 +0530

    First Commit

---
+++ format.py
@@ -0,0 +1,60 @@
+import os
+import string
+import sys
+
+path =  "latest/tungsten_off_wc_results/"
+files = os.listdir(path)
+# f2 = []
+# for f in files:
+# 	f2.append(path+f)
+# f2.sort(key=os.path.getctime)
+def real_value(x):
+	convert = {}
+	convert["million"]=1000000
+	convert["billion"]=1000000000
+	x = string.split(x,'.')[0]
+	x = string.split(x,'_')[1:]
+	num = int(x[0])
+	if(len(x)==2):
+		num *= convert[x[1]]
+	return num
+
+# files.sort(key=real_value)
+
+parameters = []
+data = []
+headers = []
+
+f = open(path+files[0])
+for line in f:
+	parameters.append(line.split('\t')[1])
+	data.append(line.split('\t')[1])
+# print len(data)
+
+def push(list):
+	for i in range(0,len(data)):
+		# print i
+		data[i] += '\t'+list[i]
+
+print files
+for file in files:
+	f = open(path+file,'r')
+	temp = []
+	for line in f:
+		temp.append(line.split('\t')[2][:-1])
+	# print temp
+	push(temp)
+	f.close()
+
+for i in files:
+	temp = string.split(i,'.')[0]
+	headers.append(temp[4:])
+
+data[0] = '\t'.join(headers)
+data = data[:83]
+output = string.split(path,'/')[-2]
+o = open(output+".csv",'w')
+for line in data:
+	o.write(str(line))
+	o.write('\n')
+o.close()
---
+++ parse.py
@@ -0,0 +1,62 @@
+import os
+import img2pdf
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import sys
+import string
+
+name=sys.argv[1].split('.')[1][1:]
+print("name="+name)
+
+path = "C:\\Users\\srama\\Documents\\Big Data\\ccbd\\"
+file = open(path+sys.argv[1],'r')
+data = []
+
+headers = []
+h = 1
+
+for i in file:
+	s=i[:-1]
+	data.append(s.split('\t'))
+file.close()
+
+headers = data[0]
+data = data[1:-1]
+
+headers
+
+n = len(headers)
+y = [i for i in range(0,n)]
+n
+y
+
+with PdfPages(path+'test.pdf') as pdf:
+	for i in data:
+		title = i[0]
+		val = i[1:]
+		# OFF = i[6:]
+
+		print(title,val)
+		for num in range(0,n):
+			val[num] = eval(val[num])
+
+		# print(y)
+		# break
+
+		plt.clf()
+
+		plt.bar(y, val, align='center', alpha=0.5)
+		plt.xticks(y, headers,rotation=90)
+		plt.ylabel(title)
+		plt.title(name)
+
+
+		# plt.bar(y,ON,label="Tungsten ON")
+		# plt.plot(y,OFF,label="Tungsten OFF")
+		# plt.xlabel('Benchmark Size', fontsize=14)
+		# plt.ylabel(title, fontsize=14)
+		# plt.xticks([0,1,2,3,4],['1B','2B','3B','4B','5B'])
+		# plt.grid(axis='y', linestyle='-')
+		# plt.legend()
+		plt.tight_layout()
+		pdf.savefig()
